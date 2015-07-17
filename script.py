3b6e5f3386ffb7e6a88ccd582b1bbad344449499
Jemals <max.a.kolle@gmail.com>
2015-07-17 20:46:22 +0000

    It works better.
    Output itself is a little weird in terms of significant figures; however, this can easily be sorted by changing the second argument to all relevant round() calls.

--- script.py
+++ script.py
@@ -22,26 +22,36 @@ overall_unc_T = combo_list( cented_reading_T,
                           cented_random_T
                         )
 
+overall_Tsqr = powerFix( overall_unc_T , 2)
+'''
 print("Raw Data Averages: " + str(raw_average_T))
 
 print("\nCalibration: " + str(get_calibration_unc(raw_average_T)))
 
 print("\nReading: " + str(get_reading_unc(raw_average_T, 0.01)))
 
-print("\nRandom: " + str(get_random_unc(raw_repeated_T)))
+print("\nRandom: " + str(powerFix(get_random_unc(raw_repeated_T), 2) ))
+'''
 
-print("""\nAverage T values:
+print("""
+         Average T values:
          {0}
+         +--------------------------+
          Percent Reading:
          {1}
          Percent Calibration:
          {2}
          Percent Random:
          {3}
-         Percent Overall:
-         {4}""".format(raw_average_T,
+         +--------------------------+
+         Percent Overall (T):
+         {4}
+         +--------------------------+
+         Percent Overall (T^2):
+         {5}""".format(raw_average_T,
                        cented_reading_T,
                        cented_calibration_T,
                        cented_random_T,
-                       overall_unc_T
+                       overall_unc_T,
+                       overall_Tsqr
                       ))
